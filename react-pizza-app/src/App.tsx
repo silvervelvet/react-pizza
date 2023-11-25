3ff89001b9179aaa44629a7b8db46262b3cf4f79
silvervelvet <108361662+silvervelvet@users.noreply.github.com>
2023-11-25 13:15:19 +0300

    refactoring

--- react-pizza-app/src/App.tsx
+++ react-pizza-app/src/App.tsx
@@ -1,41 +1,17 @@
 import React, { useEffect, useState } from 'react';
 import './scss/app.scss';
 import { Header } from './components/Header';
-import { Sort } from './components/Sort';
-import { Categories } from './components/Categories';
-import { ItemBlock } from './components/ItemBlock';
-import pizzas from '../src/assets/pizza.json'
-
+import { Home } from './pages/Home';
+import { NotFound } from './pages/NotFound';
 
 function App() {
-
-  const[items, setItems] = useState([]);
-
-  useEffect(() => {
-    fetch('https://655f9061879575426b458852.mockapi.io/items')
-    .then((res) => {
-      return res.json;
-    })
-    .then((arr) => {
-      setItems(arr);
-    })
-  }, [])
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {
-              items.map(o => <ItemBlock key={o.id} {...o}/>)
-            }
-          </div>
+          <Home />
         </div>
       </div>
     </div>
---
+++ react-pizza-app/src/pages/Home.tsx
@@ -0,0 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import './scss/app.scss';
+import { Sort } from '../components/Sort';
+import { Categories } from '../components/Categories';
+import { ItemBlock } from '../components/ItemBlock';
+
+export const Home = () => {
+
+    const[items, setItems] = useState([]);
+
+    useEffect(() => {
+      fetch('https://655f9061879575426b458852.mockapi.io/items')
+      .then((res) => {
+        return res.json;
+      })
+      .then((arr) => {
+        setItems(arr);
+      })
+    }, [])
+
+    return (
+        <>
+            <div className="content__top">
+                <Categories />
+                <Sort />
+            </div>
+            <h2 className="content__title">Все пиццы</h2>
+            <div className="content__items">
+                {
+                items.map(o => <ItemBlock key={o.id} {...o}/>)
+                }
+            </div>
+        </>
+    )
+}
